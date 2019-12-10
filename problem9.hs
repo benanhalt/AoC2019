95719b9352445dace35ed659e26be35c964adad5
Ben Anhalt <ben.anhalt@gmail.com>
2019-12-09 19:38:25 -0600

    finished day 9.

--- problem9.hs
+++ problem9.hs
@@ -5,7 +5,9 @@ import Data.List (permutations, sortOn, foldl')
 import Data.Maybe (fromMaybe)
 import qualified Data.Map.Strict as Map
 
-(!!!) :: Map.Map Integer Integer -> Integer -> Integer
+type Mem = Map.Map Integer Integer
+
+(!!!) :: Mem -> Integer -> Integer
 mem !!! i = fromMaybe 0 $ Map.lookup i mem
 
 newtype OpCode = OpCode Integer
@@ -16,65 +18,30 @@ data Status = Running | Halted | WaitingForInput | Error
 
 data State = State
   { pc :: Integer
-  , mem :: Map.Map Integer Integer
+  , mem :: Mem
+  , relBase :: Integer
   , status :: Status
   , inputs :: [Integer]
   , outputs :: [Integer]
   } deriving (Show)
 
-program = [3,8,1001,8,10,8,105,1,0,0,21,42,51,60,77,94,175,256,337,418,99999,3,9,1001,9,4,9,102,5,9,9,1001,9,3,9,102,5,9,9,4,9,99,3,9,102,2,9,9,4,9,99,3,9,1001,9,3,9,4,9,99,3,9,101,4,9,9,1002,9,4,9,101,5,9,9,4,9,99,3,9,1002,9,5,9,101,3,9,9,102,2,9,9,4,9,99,3,9,1001,9,1,9,4,9,3,9,1002,9,2,9,4,9,3,9,102,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,101,2,9,9,4,9,3,9,101,2,9,9,4,9,3,9,101,2,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,101,1,9,9,4,9,99,3,9,1001,9,2,9,4,9,3,9,101,1,9,9,4,9,3,9,102,2,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,101,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,1,9,4,9,3,9,101,1,9,9,4,9,3,9,1001,9,2,9,4,9,3,9,1002,9,2,9,4,9,99,3,9,101,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,101,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,102,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,101,2,9,9,4,9,3,9,1001,9,2,9,4,9,3,9,101,2,9,9,4,9,99,3,9,101,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,1,9,4,9,3,9,101,1,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,1002,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,101,1,9,9,4,9,3,9,102,2,9,9,4,9,3,9,101,2,9,9,4,9,99,3,9,1001,9,2,9,4,9,3,9,101,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,101,2,9,9,4,9,3,9,101,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,102,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,101,1,9,9,4,9,99]
+program = [1102,34463338,34463338,63,1007,63,34463338,63,1005,63,53,1102,1,3,1000,109,988,209,12,9,1000,209,6,209,3,203,0,1008,1000,1,63,1005,63,65,1008,1000,2,63,1005,63,904,1008,1000,0,63,1005,63,58,4,25,104,0,99,4,0,104,0,99,4,17,104,0,99,0,0,1101,0,0,1020,1101,34,0,1004,1101,0,26,1008,1102,1,37,1011,1101,39,0,1018,1102,587,1,1022,1101,1,0,1021,1102,22,1,1012,1101,0,33,1014,1101,24,0,1016,1101,0,752,1029,1101,36,0,1002,1101,35,0,1006,1101,32,0,1009,1102,38,1,1003,1102,584,1,1023,1101,0,20,1001,1102,892,1,1025,1102,29,1,1000,1101,411,0,1026,1102,1,901,1024,1101,0,761,1028,1101,23,0,1017,1102,30,1,1013,1101,0,27,1015,1102,28,1,1005,1101,408,0,1027,1101,25,0,1007,1102,31,1,1019,1101,0,21,1010,109,5,1207,-2,39,63,1005,63,199,4,187,1105,1,203,1001,64,1,64,1002,64,2,64,109,12,21102,40,1,-1,1008,1016,40,63,1005,63,229,4,209,1001,64,1,64,1106,0,229,1002,64,2,64,109,-5,1207,-5,24,63,1005,63,249,1001,64,1,64,1106,0,251,4,235,1002,64,2,64,109,-14,2102,1,6,63,1008,63,32,63,1005,63,271,1106,0,277,4,257,1001,64,1,64,1002,64,2,64,109,2,1202,1,1,63,1008,63,20,63,1005,63,303,4,283,1001,64,1,64,1106,0,303,1002,64,2,64,109,7,2108,34,2,63,1005,63,319,1106,0,325,4,309,1001,64,1,64,1002,64,2,64,109,6,2101,0,-6,63,1008,63,24,63,1005,63,349,1001,64,1,64,1105,1,351,4,331,1002,64,2,64,109,4,21107,41,42,0,1005,1017,369,4,357,1105,1,373,1001,64,1,64,1002,64,2,64,109,5,21101,42,0,-5,1008,1017,41,63,1005,63,397,1001,64,1,64,1106,0,399,4,379,1002,64,2,64,109,9,2106,0,-4,1106,0,417,4,405,1001,64,1,64,1002,64,2,64,109,-20,21108,43,43,0,1005,1011,435,4,423,1105,1,439,1001,64,1,64,1002,64,2,64,109,-15,2102,1,8,63,1008,63,34,63,1005,63,465,4,445,1001,64,1,64,1105,1,465,1002,64,2,64,109,3,1201,6,0,63,1008,63,28,63,1005,63,491,4,471,1001,64,1,64,1106,0,491,1002,64,2,64,109,18,21108,44,46,0,1005,1017,511,1001,64,1,64,1106,0,513,4,497,1002,64,2,64,109,12,1205,-8,527,4,519,1105,1,531,1001,64,1,64,1002,64,2,64,109,-17,1208,-3,32,63,1005,63,553,4,537,1001,64,1,64,1105,1,553,1002,64,2,64,109,-13,1208,10,31,63,1005,63,573,1001,64,1,64,1105,1,575,4,559,1002,64,2,64,109,17,2105,1,7,1105,1,593,4,581,1001,64,1,64,1002,64,2,64,109,-8,2107,19,-7,63,1005,63,615,4,599,1001,64,1,64,1105,1,615,1002,64,2,64,109,4,1206,8,629,4,621,1106,0,633,1001,64,1,64,1002,64,2,64,109,-2,2101,0,-6,63,1008,63,34,63,1005,63,655,4,639,1105,1,659,1001,64,1,64,1002,64,2,64,109,10,1205,0,671,1105,1,677,4,665,1001,64,1,64,1002,64,2,64,109,-21,2107,26,8,63,1005,63,693,1106,0,699,4,683,1001,64,1,64,1002,64,2,64,109,19,1201,-9,0,63,1008,63,30,63,1005,63,719,1105,1,725,4,705,1001,64,1,64,1002,64,2,64,109,9,1206,-6,741,1001,64,1,64,1106,0,743,4,731,1002,64,2,64,109,-5,2106,0,6,4,749,1001,64,1,64,1105,1,761,1002,64,2,64,109,-14,1202,-1,1,63,1008,63,27,63,1005,63,781,1105,1,787,4,767,1001,64,1,64,1002,64,2,64,109,1,21107,45,44,5,1005,1014,807,1001,64,1,64,1105,1,809,4,793,1002,64,2,64,109,8,21101,46,0,0,1008,1017,46,63,1005,63,835,4,815,1001,64,1,64,1106,0,835,1002,64,2,64,109,-26,2108,20,10,63,1005,63,857,4,841,1001,64,1,64,1106,0,857,1002,64,2,64,109,24,21102,47,1,-5,1008,1010,46,63,1005,63,881,1001,64,1,64,1106,0,883,4,863,1002,64,2,64,109,6,2105,1,3,4,889,1001,64,1,64,1105,1,901,4,64,99,21102,27,1,1,21101,915,0,0,1105,1,922,21201,1,29830,1,204,1,99,109,3,1207,-2,3,63,1005,63,964,21201,-2,-1,1,21101,0,942,0,1105,1,922,21202,1,1,-1,21201,-2,-3,1,21102,1,957,0,1105,1,922,22201,1,-1,-2,1105,1,968,21201,-2,0,-2,109,-3,2106,0,0]
 
 main :: IO ()
 main = do
-  forM_ (sortOn snd possibleThrusts) $ \t ->
-    putStrLn $ show t
+  print $ getOutput $ execState $ state0 program [1]
+  print $ getOutput $ execState $ state0 program [2]
 
-possibleThrusts :: [([Phase], Integer)]
-possibleThrusts = do
-  phases <- permutations $ Phase <$> [5 .. 9]
-  let (Input thrust, _) = execSequence phases
-  pure (phases, thrust)
+state0 :: [Integer] -> [Integer] -> State
+state0 program inputs = State
+    { pc = 0
+    , mem = Map.fromList $ zip [0 ..] program
+    , relBase = 0
+    , status = Running
+    , inputs = inputs
+    , outputs = []
+    }
 
-
-execSequence :: [Phase] -> SequenceState
-execSequence phases = head finished
-  where
-    sequenceState0 = (Input 0, startAmplifier <$> phases)
-    (running, finished) = break isFinished $ iterate runSequence sequenceState0
-
-isFinished :: SequenceState -> Bool
-isFinished (_, states) = (status $ last states) == Halted
-
-type SequenceState = (Input, [State])
-
-runSequence :: SequenceState -> SequenceState
-runSequence (input, states) = (output, states')
-  where
-    states' = runAmplifierSequence input states []
-    output = getOutput $ last states'
-
-
-runAmplifierSequence :: Input -> [State] -> [State] -> [State]
-runAmplifierSequence input toRun finished =
-  case toRun of
-    [] -> finished
-    (state : toRun') -> runAmplifierSequence output toRun' (finished <> [state'])
-      where
-        state' = runAmplifier state input
-        output = getOutput state'
-
-getOutput :: State -> Input
-getOutput (State {outputs}) = Input $ head outputs
-
-newtype Phase = Phase Integer deriving (Show)
-newtype Input = Input Integer deriving (Show)
-
-startAmplifier :: Phase -> State
-startAmplifier (Phase phase) =
-  execState $ State {pc=0, mem=Map.fromList $ zip [0..] program, status=Running, inputs=[phase], outputs=[]}
-
-runAmplifier :: State -> Input -> State
-runAmplifier state0 (Input input) = execState $ provideInput input $ state0
 
 execState :: State -> State
 execState state0 = stateF
@@ -92,6 +59,8 @@ provideInput i s@(State {inputs, status}) = s {inputs=inputs <> [i], status=stat
       WaitingForInput -> Running
       _ -> status
 
+getOutput :: State -> [Integer]
+getOutput (State {outputs}) = reverse outputs
 
 
 step :: State -> State
@@ -107,6 +76,7 @@ step s@(State {pc, mem, status}) =
         (OpCode 6, modes) -> jmpIfFalseOp modes s
         (OpCode 7, modes) -> ltOp modes s
         (OpCode 8, modes) -> eqOp modes s
+        (OpCode 9, modes) -> adjRelBaseOp modes s
         (OpCode 99, modes) -> s {status = Halted}
         _ -> s {status = Error}
     _ -> s
@@ -118,29 +88,35 @@ decodeOp i = (OpCode opCode, ModeCode <$> modeCodes)
     modeCodes = fmap (`rem` 10) $ iterate (`div` 10) $ i `div` 100
 
 readOperand :: [ModeCode] -> Int -> State -> Integer
-readOperand modes i (State {pc, mem}) =
+readOperand modes i (State {pc, mem, relBase}) =
   case modes !! i of
     ModeCode 0 -> mem !!! val  -- position mode
-    ModeCode 1 -> val         -- immediate mode
+    ModeCode 1 -> val          -- immediate mode
+    ModeCode 2 -> mem !!! (val + relBase) -- relative mode
   where
     val = mem !!! (pc + 1 + toInteger i)
+
+writeOperand :: [ModeCode] -> Int -> Integer -> State -> Mem
+writeOperand modes i val s@(State {pc, mem, relBase}) = Map.insert k val mem
+  where
+    j = mem !!! (pc + 1 + toInteger i)
+    k = case modes !! i of
+      ModeCode 0 -> j           -- position mode
+      ModeCode 2 -> j + relBase -- relative mode
 
 addOp :: [ModeCode] -> State -> State
 addOp modes s@(State {pc, mem}) = s {pc=pc + 4, mem=mem'}
   where
     a = readOperand modes 0 s
     b = readOperand modes 1 s
-    r = mem !!! (pc + 3)
-    mem' = updateAt r (a + b) mem
+    mem' = writeOperand modes 2 (a + b) s
 
 multOp :: [ModeCode] -> State -> State
 multOp modes s@(State {pc, mem}) = s {pc=pc + 4, mem=mem'}
   where
     a = readOperand modes 0 s
     b = readOperand modes 1 s
-    r = mem !!! (pc + 3)
-    aTimesb = a * b
-    mem' = updateAt r aTimesb mem
+    mem' = writeOperand modes 2 (a * b) s
 
 inOp :: [ModeCode] -> State -> State
 inOp modes s@(State {pc, mem, inputs}) =
@@ -149,8 +125,7 @@ inOp modes s@(State {pc, mem, inputs}) =
     _ -> s {pc=pc + 2, mem=mem', inputs=tail inputs}
       where
         input = head inputs
-        a = mem !!! (pc + 1)
-        mem' = updateAt a input mem
+        mem' = writeOperand modes 0 input s
 
 outOp :: [ModeCode] -> State -> State
 outOp modes s@(State {pc, mem, outputs}) = s {pc=pc + 2, outputs=(a : outputs)}
@@ -176,19 +151,18 @@ ltOp modes s@(State {pc, mem}) = s {pc=pc + 4, mem=mem'}
   where
     a = readOperand modes 0 s
     b = readOperand modes 1 s
-    r = mem !!! (pc + 3)
     lt = if a < b then 1 else 0
-    mem' = updateAt r lt mem
+    mem' = writeOperand modes 2 lt s
 
 eqOp :: [ModeCode] -> State -> State
 eqOp modes s@(State {pc, mem}) = s {pc=pc + 4, mem=mem'}
   where
     a = readOperand modes 0 s
     b = readOperand modes 1 s
-    r = mem !!! (pc + 3)
     eq = if a == b then 1 else 0
-    mem' = updateAt r eq mem
+    mem' = writeOperand modes 2 eq s
 
-updateAt :: Integer -> a -> Map.Map Integer a -> Map.Map Integer a
-updateAt = Map.insert
-
+adjRelBaseOp :: [ModeCode] -> State -> State
+adjRelBaseOp modes s@(State {pc, relBase}) = s {pc=pc + 2, relBase=relBase + a}
+  where
+    a = readOperand modes 0 s
